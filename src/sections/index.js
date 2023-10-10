ef8c6f19f7b1df19e706274bf7f76b326f78a2a0
Stevens Bougault <stevensbougault@Stevenss-MacBook-Pro.local>
2023-10-10 01:54:48 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./popularProducts";
+import Services from "./services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
